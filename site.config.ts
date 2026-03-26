6237fb5d3ab73a5c64d5bb51bdd78ec54399dcf3
muharlabs <mrmahoney34@gmail.com>
2026-03-26 19:27:22 +0300

    fisrt commit - Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '32f4b186be7280f79fc4e30f935c9c58',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'From Ants to Stars',
+  domain: 'vercel website',
+  author: 'Mr Nobody',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
